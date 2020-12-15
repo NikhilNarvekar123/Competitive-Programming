b12a24b9204b4e891d6f96ed1e003df33441d3bc
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2020-12-15 16:02:26 -0600

    Update vertex.py

--- aStarAlgorithm/vertex.py
+++ aStarAlgorithm/vertex.py
@@ -5,10 +5,14 @@ class Vertex:
         self.value = value
         self.name = name
         self.adjacentVertices = []
+        self.heuristic_dist = 0
 
     def set_adjacent_vertices(self, *args):
         for arg in args:
             self.adjacentVertices.append(arg)
+
+    def set_heuristic_dist(self, dist : int):
+        self.heuristic_dist = dist
 
     def get_value(self) -> int:
         return self.value
@@ -17,7 +21,10 @@ class Vertex:
         return self.name
 
     def get_adjacent_vertices(self) -> list:
-        return self.adjacentVertices
+        vertices = []
+        for vertex in self.adjacentVertices:
+            vertices.append(vertex[0])
+        return vertices
 
     def display_adjacent_vertices(self) -> str:
         dispStr = ''
@@ -28,11 +35,15 @@ class Vertex:
                 dispStr += ' - '
         return dispStr
 
+    def get_heuristic_dist(self) -> int:
+        return self.heuristic_dist
 
     def get_edge_weight(self, vertex) -> int:
         for v in self.adjacentVertices:
             if(vertex == v[0]):
                 return v[1]
+        if(vertex == self):
+            return 0
         return -1
 
 # driver code
