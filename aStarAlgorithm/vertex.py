391ac50b948a463de50fff9f82b56c749199864c
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2020-12-15 00:30:09 -0600

    Update vertex.py

--- aStarAlgorithm/vertex.py
+++ aStarAlgorithm/vertex.py
@@ -6,30 +6,30 @@ class Vertex:
         self.name = name
         self.adjacentVertices = []
 
-    def setAdjacentVertices(self, *args):
+    def set_adjacent_vertices(self, *args):
         for arg in args:
             self.adjacentVertices.append(arg)
 
-    def getValue(self) -> int:
+    def get_value(self) -> int:
         return self.value
 
-    def getName(self) -> str:
+    def get_name(self) -> str:
         return self.name
 
-    def getAdjacentVertices(self) -> list:
+    def get_adjacent_vertices(self) -> list:
         return self.adjacentVertices
 
     def display_adjacent_vertices(self) -> str:
         dispStr = ''
         for i in range(len(self.adjacentVertices)):
             vertex = self.adjacentVertices[i]
-            dispStr += '(' + vertex[0].getName() + ',' + str(vertex[1]) + ')'
+            dispStr += '(' + vertex[0].get_name() + ',' + str(vertex[1]) + ')'
             if i != (len(self.adjacentVertices) - 1):
                 dispStr += ' - '
         return dispStr
 
 
-    def getEdgeWeight(self, vertex) -> int:
+    def get_edge_weight(self, vertex) -> int:
         for v in self.adjacentVertices:
             if(vertex == v[0]):
                 return v[1]
@@ -38,6 +38,6 @@ class Vertex:
 # driver code
 # v = Vertex(1, "A")
 # e = Vertex(2, "E")
-# v.setAdjacentVertices((e,55))
-# print(v.getEdgeWeight(e))
-# print(v.getName())
+# v.set_adjacent_vertices((e,55))
+# print(v.get_edge_weight(e))
+# print(v.get_name())
