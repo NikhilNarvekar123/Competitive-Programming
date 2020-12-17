fe1e7c5de287a74761c9eec37e8f643e9e78714a
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2020-12-16 20:05:00 -0600

    Create swapNodesInPairs.py
    personal solution which uses recursion, same logic as iterative solutions but much slower due to recursive calls

---
+++ swapNodesInPairs.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    
+    def swapPairs(self, head: ListNode) -> ListNode:
+        
+    
+        
+        if not head or not head.next:
+            return head
+        
+        newHead = head.next
+        
+        nextNode = newHead.next
+        nextNode = self.swapPairs(nextNode)
+        
+        head.next = nextNode
+        
+        newHead.next = head
+        head = newHead
+        
+    
+        
+        return head
+    
