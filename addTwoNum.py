484621c60c09ec93a7fc723b51198cb61db6da72
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2021-09-06 02:01:50 -0500

    Update addTwoNum.py

--- addTwoNum.py
+++ addTwoNum.py
@@ -1,70 +1,48 @@
+'''
+run-time: 44ms, faster than 99.52%
+mem-usage: 13.2 MB, less than 99.06%
+'''
+
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
-
-class Solution:
-   
-
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
         
-        fin = ListNode()
-        l3 = fin
         carry = 0
-        firstLoop = True
+        resultSum = ListNode()
+        tempSum = resultSum # reference to keep adding to result linked list
         
-        while l1 != None or l2 != None:
+        while l1 or l2:
             
-            num1 = l1.val if l1 != None else 0
-            num2 = l2.val if l2 != None else 0
-            l1 = l1.next if l1 != None else None 
-            l2 = l2.next if l2 != None else None
+            digitSum = carry
+            carry = 0
             
-            numSum = num1 + num2 + carry
+            if l1:
+                digitSum += l1.val
+                l1 = l1.next
             
-            carry = numSum // 10
+            if l2:
+                digitSum += l2.val
+                l2 = l2.next
+                
+            if digitSum >= 10:
+                carry = 1
+                digitSum -= 10
             
-            l3.next = ListNode(numSum % 10, None)
-            l3 = l3.next
-            if firstLoop:
-                fin = l3
-                firstLoop = False
+            tempSum.val = digitSum
+            if l1 or l2:
+                tempSum.next = ListNode()
+                tempSum = tempSum.next
+                
+        if carry == 1:
+            tempSum.next = ListNode(1, None)
         
-        if carry != 0:
-            l3.next = ListNode(1, None)
-            l3 = l3.next
-        
-        return fin
-
-
-    '''
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
-        num1 = ""
-        num2 = ""
-        finNum = 0
-        
-        while l1 != None:
-            num1 = str(l1.val) + num1
-            l1 = l1.next 
-        
-        while l2 != None:
-            num2 = str(l2.val) + num2
-            l2 = l2.next 
-        
-        finNum = int(num1) + int(num2)
-        
-        tempStr = str(finNum)
-        
-        l1 = ListNode(int(tempStr[len(tempStr) - 1]), self.recur(2, tempStr))  
-            
-        return l1
-    
-    
-    def recur(self, val, tempStr) -> ListNode:
-        if(len(tempStr) - val >= 0):
-            return ListNode(int(tempStr[len(tempStr) - val]), self.recur(val + 1, tempStr))
-            
-    '''
+        return resultSum
