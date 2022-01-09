290a6c5c2a2584287ced186e41b28e04a4c40c31
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2022-01-09 17:28:18 -0600

    Update reverse_string.py

--- reverse_string.py
+++ reverse_string.py
@@ -1,13 +1,24 @@
 '''
-O(n)
+O(n/2)
 '''
 
 class Solution:
     def reverseString(self, s: List[str]) -> None:
+    
+        def reverse(s, i, j):
+            if i >= j:
+                return
+            s[i], s[j] = s[j], s[i]
+            reverse(s, i + 1, j - 1)
         
+        reverse(s, 0, len(s) - 1)
+        
+        # iterative
+        '''
         i = 0
         j = len(s) - 1
         while i < j:
             s[i], s[j] = s[j], s[i]
             i += 1
             j -= 1
+        '''
