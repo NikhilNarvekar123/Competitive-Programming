0af756d9e510e2b782929d752558ef18e233d5ef
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2022-01-17 17:53:56 -0600

    Update mostWaterContainer.py

--- mostWaterContainer.py
+++ mostWaterContainer.py
@@ -1,44 +1,21 @@
+'''
+O(n)
+'''
+
 class Solution:
-    
     def maxArea(self, height: List[int]) -> int:
         
-        
-        j = len(height) - 1
         i = 0
-        maxArea = 0
+        j = len(height) - 1
         
-        while i != j:
-            x = abs(j - i)
-            y = height[j] if height[j] < height[i] else height[i]
-            maxArea = x * y if x * y > maxArea else maxArea
-            
-            if height[j] > height[i]:
-                i += 1
-            elif height[j] < height[i]:
+        maxVol = -1
+        while i < j:
+
+            vol = (j - i) * min(height[i], height[j])
+            maxVol = max(vol, maxVol)
+            if height[i] > height[j]:
                 j -= 1
             else:
                 i += 1
         
-        return maxArea
-            
-            
-        
-            
-        
-        
-        ''' old solution
-        maxArea = 0
-        
-        for i in range(0, len(height) - 1):
-            
-            num1 = height[i]
-            
-            for j in range(i + 1, len(height)):
-                num2 = height[j]
-                
-                area = j - i
-                area = area * num1 if num1 < num2 else area * num2
-                maxArea = area if area > maxArea else maxArea  
-        
-        return maxArea
-        '''
+        return maxVol
