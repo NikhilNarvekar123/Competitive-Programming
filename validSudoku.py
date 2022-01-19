7dda6fa45498610f97ecc95c3b24a564f638ba71
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2022-01-19 17:13:50 -0600

    Update validSudoku.py

--- validSudoku.py
+++ validSudoku.py
@@ -1,116 +1,24 @@
+'''
+O(m * n)
+'''
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        rowCount = set()
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
         
-
-        for r in range(0 , len(board)):   
-            
-            for c in range(0, len(board)):
-                
-                val = board[r][c]
-                print(val)
-                
-                if(val == '.'):
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == '.':
                     continue
-                
-                if((val + " r: " + str(r)) in rowCount):
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or 
+                    board[r][c] in squares[(r // 3, c // 3)]):
                     return False
-                else:
-                    rowCount.add(val + " r: " + str(r))
-                
-                if((val + " c: " + str(c)) in rowCount):
-                    return False
-                else:
-                    rowCount.add(val + " c: " + str(c))
-                
-                if((val + " b: " + str(r//3) + " " + str(c//3)) in rowCount):
-                    return False
-                else:
-                    rowCount.add(val + " b: " + str(r//3) + " " + str(c//3))
-                
-                
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
         
         return True
-        
-        
-        
-        
-        
-        
-        
-        '''
-                rowCount = [{'1' : 0, '2' : 0, '3' : 0,'4' : 0,'5' : 0,'6' : 0,'7' : 0, '8' : 0, '9' : 0 }] * 9
-            colCount = [{'1' : 0, '2' : 0, '3' : 0,'4' : 0,'5' : 0,'6' : 0,'7' : 0, '8' : 0, '9' : 0 }] * 9
-            boxCount = [[{'1' : 0, '2' : 0, '3' : 0,'4' : 0,'5' : 0,'6' : 0,'7' : 0, '8' : 0, '9' : 0 }] * 3] * 3
-      
-        
-                row = rowCount[r]
-                col = colCount[c]
-                box = boxCount[r // 3][c // 3]
-                
-                if(row[val] != 0):
-                    print(row)
-                    return False
-                else:
-                    row[val] = 1
-                    print(row)
-                    
-                if(col[val] != 0):
-                    print("col")
-                    return False
-                else:
-                    col[val] = 1
-        
-                if(box[val] != 0):
-                    print("box")
-                    return False
-                else:
-                    box[val] = 1
-        
-        
-        '''
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        '''
-        
-        
-        
-        for row in board:
-            
-            for col in row:
-                if(col == "."):
-                    continue
-                elif(rowCount[col] != 0):
-                    return False
-                elif(rowCount[col] == 0):
-                    rowCount[col] = 1
-                
-            rowCount = {'1' : 0, '2' : 0, '3' : 0,'4' : 0,'5' : 0,'6' : 0,'7' : 0, '8' : 0, '9' : 0, }
-    
-        rowCount = {'1' : 0, '2' : 0, '3' : 0,'4' : 0,'5' : 0,'6' : 0,'7' : 0, '8' : 0, '9' : 0, }
-        
-        for col in range(len(board)):
-            
-            for row in range(len(board)):
-                val = board[row][col]
-                if(val == "."):
-                    continue
-                elif(rowCount[val] != 0):
-                    return False
-                elif(rowCount[val] == 0):
-                    rowCount[val] = 1
-                    
-            rowCount = {'1' : 0, '2' : 0, '3' : 0,'4' : 0,'5' : 0,'6' : 0,'7' : 0, '8' : 0, '9' : 0, }
-        
-        return True
-        '''
