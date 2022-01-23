a20c227014155746a8ae2d489ec7d8974f4c4e09
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2022-01-22 19:32:08 -0600

    Update valid_anagram.py

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,33 +1,21 @@
 '''
-run-time: 52 ms (faster than 60.77%)
-mem-usage: 14.5 mb (less than 81.61%)
 O(n)
 '''
 
 class Solution:
-    
     def isAnagram(self, s: str, t: str) -> bool:
         
-        
-        if len(t) != len(s):
+        if len(s) != len(t):
             return False
         
-        charMap = {}
+        repMap = collections.defaultdict(int)
         
-        for c in s:
-            if c in charMap:
-                charMap[c] += 1
-            else:
-                charMap[c] = 1
+        for i in range(len(s)):
+            repMap[s[i]] += 1
+            repMap[t[i]] -= 1
         
-        for c in t:
-            if c in charMap:
-                charMap[c] -= 1
-        
-        
-        for key, val in charMap.items():
+        for key, val in repMap.items():
             if val != 0:
                 return False
-            
-        return True
         
+        return True
