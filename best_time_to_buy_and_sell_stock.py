0b78ddc0e96ca0fd12f769e8195ee0e48d82f239
Nikhil Narvekar <31781542+NikhilNarvekar123@users.noreply.github.com>
2022-01-25 21:43:08 -0600

    Update best_time_to_buy_and_sell_stock.py

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -1,31 +1,18 @@
 '''
-run-time: 1104 ms (faster than 59.55%)
-mem-usage: 25.2 mb (faster than 11.92%)
-O(n) algorithm
+O(n)
+Kandane's algo but modified so array being summed is array of differences between consecutive indices
 '''
 
-
 class Solution:
-    
     def maxProfit(self, prices: List[int]) -> int:
-    
-    
-        # O(n^2)
-        # maxProfit = 0
-        # for i in range(len(prices) - 1):            
-        #     for j in range(i + 1, len(prices)):
-        #         maxProfit = max(maxProfit, prices[j] - prices[i])
-        # return maxProfit
         
-        # O(n)
-        maxProfit = 0
-        minPrice = prices[0]
+        curSum = 0
+        maxSum = 0
         
-        for i in range(1, len(prices)):
-            minPrice = min(prices[i], minPrice)
-            maxProfit = max(prices[i] - minPrice, maxProfit)
-        
-        return maxProfit
-            
-            
-        
+        for i in range(len(prices) - 1):
+            curSum += prices[i + 1] - prices[i]
+            if curSum < 0:
+                curSum = 0
+            maxSum = max(curSum, maxSum)
+
+        return maxSum
